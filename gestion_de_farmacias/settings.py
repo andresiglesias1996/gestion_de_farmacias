2907020487fbbdc47cc43b4d859275d5f2b75238
andresiglesias1996 <92554302+andresiglesias1996@users.noreply.github.com>
2021-10-18 19:58:45 -0300

    AndresI

--- gestion_de_farmacias/settings.py
+++ gestion_de_farmacias/settings.py
@@ -81,15 +81,15 @@ WSGI_APPLICATION = 'gestion_de_farmacias.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
-"""
+
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
-"""
 
+"""
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
@@ -100,7 +100,7 @@ DATABASES = {
         'PORT': '5432', #'DATABASE_PORT': '5432',
     }
 }
-
+"""
 
 # Password validation
 # https://docs.djangoproject.com/en/3.2/ref/settings/#auth-password-validators
